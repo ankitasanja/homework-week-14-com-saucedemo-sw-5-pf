bb50fb6449b68c5e2cac0292c991a764531f1d3f
ankitasanja <ankitasanja11@gmail.com>
2023-05-09 10:41:20 +0100

    My homework week 6

---
+++ src/test/java/com/saucedemo/testsuite/LoginPageTest.java
@@ -0,0 +1,36 @@
+package com.saucedemo.testsuite;
+
+import com.saucedemo.customlisteners.CustomListeners;
+import com.saucedemo.pages.LoginPage;
+import com.saucedemo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(CustomListeners.class)
+public class LoginPageTest extends BaseTest {
+
+    LoginPage loginPage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void inIt(){
+        loginPage = new LoginPage();
+    }
+
+    @Test(groups = {"sanity", "regression"})
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        loginPage.loginToApplication("standard_user","secret_sauce");
+        String expectedText = "Products";
+        Assert.assertEquals(loginPage.getTextFromProductTitle(),expectedText,"Text not displayed");
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void verifyThatSixProductsAreDisplayedOnPage() {
+        loginPage.loginToApplication("standard_user","secret_sauce");
+        String expectedNumberOfProducts = "6";
+        String actualNumberOfProducts = String.valueOf(loginPage.countProductOnList());
+        Assert.assertEquals(actualNumberOfProducts,expectedNumberOfProducts,"total number of products are not displayed");
+
+    }
+}
